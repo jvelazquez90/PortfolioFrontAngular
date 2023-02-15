d3656aca00e0dcb856345d58f42450c731ef4ebf
jvelazquez <jvelazquez.tab@gmail.com>
2023-02-15 02:25:19 -0300

    Haciendo el inicio de sesion

--- src/app/componentes/iniciar-sesion/iniciar-sesion.component.ts
+++ src/app/componentes/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AutenticacionService } from 'src/app/servicios/autenticacion.service';
 
 @Component({
   selector: 'app-iniciar-sesion',
@@ -10,7 +12,7 @@ export class IniciarSesionComponent implements OnInit {
 
   form:FormGroup; // vamos a enlazar con nuestro template
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private autenticacionService: AutenticacionService, private ruta:Router) {
     this.form = this.formBuilder.group(
       {
         email: ['', [Validators.required, Validators.email]],
@@ -35,5 +37,14 @@ export class IniciarSesionComponent implements OnInit {
 
   get Password(){
     return this.form.get('password');
+  }
+
+  onEnviar(event:Event)
+  {
+    event.preventDefault;
+    this.autenticacionService.IniciarSesion(this.form.value).subscribe(data => {
+      console.log("DATA:" + JSON.stringify(data));
+      this.ruta.navigate(['/portfolio']);
+    })
   }
 }
