fd8484d2c375e783de4896fc198c6ea008b720ef
jvelazquez <jvelazquez.tab@gmail.com>
2023-03-24 19:56:37 -0300

    Se hicieron cambios sobre componente Aptitudes aunque el mismo todavia no estar corregido del todo

--- src/app/componentes/aptitudes/aptitudes.component.ts
+++ src/app/componentes/aptitudes/aptitudes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Aptitudes } from 'src/app/model/aptitudes';
+import { AptitudesService } from 'src/app/servicios/aptitudes/aptitudes.service';
 
 @Component({
   selector: 'app-aptitudes',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AptitudesComponent implements OnInit {
 
-  constructor() { }
+  aptitudes: any;
+
+
+  nombre:string = '';
+  clasificacion:string = '';
+
+  aptitudess: Aptitudes = new Aptitudes(this.nombre, this.clasificacion);
+
+  relacion: Map<string, string> = new Map<string, string>();
+  clasificaciones: Array<string> = [];
+
+  constructor( private aptitudService: AptitudesService) { }
 
   ngOnInit(): void {
+    this.cargarDatos();
+    console.log(this.aptitudes);
+  }
+
+  cargarDatos(): void {
+    const aptitudess: Aptitudes = new Aptitudes(this.nombre,this.clasificacion);
+    this.aptitudService.ObtenerAptitudes().subscribe(data => {
+      this.aptitudes = data;
+    });
+  }
+
+  obtenerInformacion( aptitudes:any ): void {
+    for( var i=0; i< aptitudes.length; i++){
+        if( aptitudes.clasificacion.includes(aptitudes[i].clasificacion) ) {
+            this.relacion.set(aptitudes[i].nombre, aptitudes[i].clasificacion);
+        }
+        else{
+          this.clasificaciones.push(aptitudes[i].clasificacion);
+        }
+    }
   }
 
 }
--- src/app/servicios/aptitudes/aptitudes.service.ts
+++ src/app/servicios/aptitudes/aptitudes.service.ts
@@ -2,17 +2,36 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'; // agregado
 import { BehaviorSubject, Observable } from 'rxjs'; // se agrega para que la aplicacion no quede en la espera del servicio
 import { map } from 'rxjs/operators';
+import { Aptitudes } from 'src/app/model/aptitudes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AptitudesService {
 
-  url = "http://localhost:8080/aptitudes/get";
+  url = "http://localhost:8080/aptitudes";
 
   constructor( private httpClient:HttpClient ) { }
 
+  //----------------------------------------------------------------------------------------
+
   ObtenerAptitudes():Observable<any>{
-    return this.httpClient.get(this.url);
+    return this.httpClient.get(this.url + "/get");
+  }
+ 
+  crearAptitudes(aptitud: any): Observable<any>{
+    return this.httpClient.post(this.url + '/crear/',aptitud);
+  }
+
+  obtenerAptitudesId(id: number): Observable<any>{
+    return this.httpClient.get(this.url + '/obtener/' + id);
+  }
+
+  editarAptitudes(aptitud: Aptitudes): Observable<any>{
+    return this.httpClient.put(this.url + '/editar/' + aptitud.id, aptitud);
+  }
+
+  eliminarAptitudes(id: number): Observable<any>{
+    return this.httpClient.delete(this.url + '/borrar/' + id);
   }
 }
