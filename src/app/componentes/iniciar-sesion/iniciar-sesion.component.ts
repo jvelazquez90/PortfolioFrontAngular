e53f49c18eac863398d8ca9082d26cf35e64d580
jvelazquez <jvelazquez.tab@gmail.com>
2023-04-18 03:03:09 -0300

    Se agrega componente iniciar sesion

--- src/app/componentes/iniciar-sesion/iniciar-sesion.component.ts
+++ src/app/componentes/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AutenticacionService } from 'src/app/servicios/autenticacion.service';
+import { User } from 'src/app/model/user';
+import { LoginService } from 'src/app/servicios/login/login.service';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-iniciar-sesion',
@@ -10,41 +12,22 @@ import { AutenticacionService } from 'src/app/servicios/autenticacion.service';
 })
 export class IniciarSesionComponent implements OnInit {
 
-  form:FormGroup; // vamos a enlazar con nuestro template
+  //form:FormGroup; // vamos a enlazar con nuestro template
 
-  constructor(private formBuilder: FormBuilder, private autenticacionService: AutenticacionService, private ruta:Router) {
-    this.form = this.formBuilder.group(
-      {
-        email: ['', [Validators.required, Validators.email]],
-        password: ['', [Validators.required, Validators.minLength(8)]],
-        deviceInfo: this.formBuilder.group({
-          deviceId: ["17867868768"],
-          deviceType: ["DEVICE_TYPE_ANDROID"],
-          notificationToken: ["67657575eececc34"]
-        })
-      }
-    )
+  user: User = new User();
+  @Output() cambiarValorLogueado = new EventEmitter<Boolean>();
+
+  constructor(private loginService: LoginService, private router:Router) {
+    
   }
 
   ngOnInit(): void {
   }
 
-  // propiedades para obtener el email y el password
-
-  get Email(){
-    return this.form.get('email');
-  }
-
-  get Password(){
-    return this.form.get('password');
-  }
-
-  onEnviar(event:Event)
-  {
-    event.preventDefault;
-    this.autenticacionService.IniciarSesion(this.form.value).subscribe(data => {
-      console.log("DATA:" + JSON.stringify(data));
-      this.ruta.navigate(['/portfolio']);
-    })
+  userLogin() {
+    this.loginService.loginUser(this.user).subscribe(data=>{
+      window.location.href = "/portfolio"
+    }, error => alert("El usuario o la contraseña son inválidos"));
+    this.cambiarValorLogueado.emit(true);
   }
 }
