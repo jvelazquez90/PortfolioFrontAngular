59c1d672d7bfca155a58c06751c5ba67821dca46
jvelazquez <jvelazquez.tab@gmail.com>
2023-04-18 03:07:09 -0300

    Se intento agregar variable logueado

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { EditableService } from 'src/app/servicios/editable/editable.service';
 import { EncabezadoComponent } from '../encabezado/encabezado.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -11,18 +12,26 @@ export class HeaderComponent implements OnInit {
 
   mostrar: Boolean = false;
   noMostrar: Boolean = true;
+  logueado: Boolean = false;
   @Output() cambiarValorMostrar = new EventEmitter<Boolean>();
   @Output() cambiarValorOcultar = new EventEmitter<Boolean>();
 
   comenzarEdicion() {
     this.cambiarValorMostrar.emit(true);
+    this.mostrar = true;
   }
 
   terminarEdicion() {
     this.cambiarValorOcultar.emit(false);
+    this.mostrar = false;
   }
 
-  constructor(private botonEditable: EditableService) { }
+  cerrarSesion() {
+    this.logueado = false;
+    this.router.navigate(['iniciar-sesion'])
+  }
+
+  constructor(private botonEditable: EditableService, private router: Router) { }
 
   ngOnInit(): void {
   }
